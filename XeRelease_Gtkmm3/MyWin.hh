33613dcd00032e59715a1f3144888be8a2364a06
daleclack <daleclack1@foxmail.com>
2021-06-22 17:10:55 +0800

    Fix compile error

---
+++ XeRelease_Gtkmm3/MyWin.hh
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <gtkmm.h>
+
+class MyWin : public Gtk::ApplicationWindow{
+public:
+    MyWin();
+private:
+    //Child Widgets
+    Glib::RefPtr<Gtk::Builder> menu_builder;
+
+    //Main window
+    Gtk::Image background;
+    Gtk::Overlay overlay;
+    Gtk::Box btn_box;
+    Gtk::ComboBoxText combo;
+    Gtk::Button btn_ver;
+
+    //TitleBar and menu
+    Gtk::HeaderBar header;
+    Gtk::MenuButton menubtn;
+    Gtk::PopoverMenu popover;
+    void titlebar_init();
+
+    //Backgrounds
+    void background1();
+    void background2();
+
+    //Signal Handlers
+    void about_dialog();
+};
