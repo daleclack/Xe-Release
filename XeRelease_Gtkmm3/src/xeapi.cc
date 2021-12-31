63fc2ff8cf48bb2bb3bf4cdb1e911642b53cdcb4
daleclack <daleclack1@foxmail.com>
2022-01-01 00:08:29 +0800

    Change xeapi

--- XeRelease_Gtkmm3/src/xeapi.cc
+++ XeRelease_Gtkmm3/src/xeapi.cc
@@ -4,6 +4,6 @@ int xeapi1(struct tm *local){
 	int year2=local->tm_year+1900,month2=local->tm_mon+1;
 	//printf("%d %d",year1,year2);
 	apiver=(year2-year1)*12-month1+month2+22;
-	if(local->tm_mday<7) apiver--;
+	//if(local->tm_mday<7) apiver--;
 	return apiver;
 }
